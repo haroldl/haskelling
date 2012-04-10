1c298d4268afc688469a77c34d6e3c15b5513f17
Harold Lee <hlee@linkedin.com>
2012-04-10 13:42:05 -0700

    Change error handling code to use Exceptions.

--- ezhttp/EZHTTP.hs
+++ ezhttp/EZHTTP.hs
@@ -1,23 +1,28 @@
-module EZHTTP (get) where
+{-# LANGUAGE DeriveDataTypeable #-}
 
+module EZHTTP (get, InvalidURLException, HttpException) where
+
+import Control.Exception (throwIO)
+import Control.Exception.Base
 import Data.Char (intToDigit)
+import Data.Typeable
 import Network.HTTP
 import Network.Stream
 import Network.URI
 
 get :: String -> IO String
 get url = case parseURI url of
-               Nothing -> fail $ "Invalid URI: " ++ url
+               Nothing -> throwIO $ InvalidURLException $ "Invalid URI: " ++ url
                Just uri -> getURI uri
 
 getURI :: URI -> IO String
 getURI uri = do response <- simpleHTTP (request uri)
                 handleResponse response
-  where handleResponse (Left  err) = fail $ "Failed request: " ++ (show err)
+  where handleResponse (Left  err) = throwIO $ HttpException $ "Failed request: " ++ (show err)
         handleResponse (Right rsp) = unpackResponse rsp
         unpackResponse rsp = case rspCode rsp of
                                   (2, 0, 0) -> return (rspBody rsp)
-                                  _ -> fail $ "Bad response code: " ++ (httpError rsp)
+                                  _ -> throwIO $ HttpException $ "Bad response code: " ++ (httpError rsp)
         httpError r = showRspCode (rspCode r) ++ " " ++ rspReason r
         showRspCode (a,b,c) = map intToDigit [a,b,c]
 
@@ -26,3 +31,13 @@ request uri = Request { rqURI     = uri,
                         rqMethod  = GET,
                         rqHeaders = [],
                         rqBody    = "" }
+
+data InvalidURLException = InvalidURLException { url :: String }
+  deriving (Show, Eq, Typeable)
+
+instance Exception InvalidURLException
+
+data HttpException = HttpException { message :: String }
+  deriving (Show, Eq, Typeable)
+
+instance Exception HttpException
--- ezhttp/Main.hs
+++ ezhttp/Main.hs
@@ -1,8 +1,18 @@
 module Main where
 
 import EZHTTP
+import Control.Exception as E
 
+main :: IO ()
 main = do html <- get "http://harold.hotelling.net/"
-          putStrLn html
-          html2 <- get "ntahoenuthantoehutnah"
-          putStrLn html2
+          putStrLn $ "Web page has " ++ (show $ length html) ++ " characters."
+          badRequest `E.catch` handleInvalidURL
+          putStrLn "Recovered from the bad value safely."
+
+badRequest :: IO ()
+badRequest = do html2 <- get "abcdefghijklmnopqrstuvwxyz"
+                putStrLn html2
+
+handleInvalidURL :: InvalidURLException -> IO ()
+handleInvalidURL e = do putStrLn message
+  where message = "caught: " ++ show (e :: InvalidURLException)
